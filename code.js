b1e4cf5ad4dd2a9f9aab7890f19fd24bbd3be8aa
katiiisha <mendeleevakate2002@gmail.com>
2022-12-07 18:04:06 +0300

    додела дз

--- code.js
+++ code.js
@@ -1,23 +1,21 @@
-// let start = 0 
-// let min = []
-// while (start<=59){
-//     min.push(start++);
-// }
-let num = prompt("Введите число от0 до 59")
-// let min = 0
-// let max = 59
-// function selfRandom(min, max)
-// {
-//   return Math.floor(Math.random() * (max - min + 1)) + min;
-// }
-// alert(`Получилось число ${num}`)
-
-if (num<=15) {
+//задача 1
+let min = prompt("Введите число от0 до 59")
+if ((min>=0)(min<=14)) {
     alert(`Введенное число попадает в первую четверть`)
-}  else if ((num>15)&&(num<=30)) {
+}  else if ((min>=15)&&(min<=30)) {
     alert(`Введенное число попадает во вторую четверь`)
-} else if ((num>30)&&(num<=45)) {
+} else if ((min>30)&&(min<=45)) {
     alert(`Введенное число попадает в третью четверть`)
-} else if ((num>45)&&(num<=59)){
+} else if ((min>45)&&(min<=59)){
     alert(`Введенное число попадает в четвертую четверть`)
 }
+//задача 2
+let lang = prompt("Если хотите вывести дни недели на русском введите - ru, если на английском - en")
+if (lang=="ru") {
+    let arr = ['понедельник', 'вторник', 'среда', 'чтверг', 'пятница', 'суббота', 'воскресенье']
+} else if (lang=="en"){
+    let arr = ['monday', 'tuesday','wednesday', 'thursday', 'friday', 'saturday', 'sunday']
+}
+alert(`${arr}`)
+
+
